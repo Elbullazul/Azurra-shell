a2c6cde44ac22aab7fd1c3ab7feed7cd2f8dd373
Christian Medel <cmedelahumada@gmail.com>
2023-03-08 13:40:08 -0500

    Add basic autogen script

---
+++ autogen.sh
@@ -0,0 +1,8 @@
+[ ! -d "$1" ] && echo "Not a valid directory: '$d'" && exit
+
+echo -n "Compiling '$1'..."
+
+sassc "$1/cinnamon.scss" >> "$1/cinnamon.css"
+# sassc "$1/gnome-shell.scss" >> "$1/gnome-shell.css"
+
+echo "done"
