919e820381a8b884c6ba475c372c0378bc62be6e
Christian Medel <cmedelahumada@gmail.com>
2023-03-13 10:56:05 -0400

    Add dark theme

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,12 @@
-[ ! -d "$1" ] && echo "Not a valid directory: '$d'" && exit
+[ ! -d "$1" ] && echo "Not a valid directory: '$1'" && exit
 
 echo -n "Compiling '$1'..."
 
 sassc "$1/cinnamon.scss" > "$1/cinnamon.css"
 # sassc "$1/gnome-shell.scss" > "$1/gnome-shell.css"
+
+if [ -f "$1/cinnamon-dark.scss" ]; then
+  sassc "$1/cinnamon-dark.scss" > "$1/cinnamon-dark.css"
+fi
 
 echo "done"
