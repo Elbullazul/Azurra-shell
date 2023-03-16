f43b0e033272538cceffe9d7b2a7663290dc34b2
Christian Medel <cmedelahumada@gmail.com>
2023-03-16 11:48:20 -0400

    Add auto deploy

--- autogen.sh
+++ autogen.sh
@@ -9,4 +9,18 @@ if [ -f "$1/cinnamon-dark.scss" ]; then
   sassc "$1/cinnamon-dark.scss" > "$1/cinnamon-dark.css"
 fi
 
+# load theme config file
+. "$1/theme.rc"
+
+# copy files
+if [ ! -z $CINNAMON_DIR ]; then
+  cp -r "$1/"*.css $CINNAMON_DIR
+  cp -r "$1/assets" $CINNAMON_DIR
+fi
+
+if [ -f "$1/cinnamon-dark.css" ] && [ ! -z $CINNAMON_DARK_DIR ]; then
+  cp -r "$1/cinnamon-dark.css" "$CINNAMON_DARK_DIR/cinnamon.css"
+  cp -r "$1/assets-dark" "$CINNAMON_DARK_DIR/assets"
+fi
+
 echo "done"
